ea64de58ea1797df41c72b14fbae2f0cc14b446d
svc-student <svc-student@student.directory.jccc>
2017-04-24 13:21:33 -0500

    master

---
+++ Assets/Nodes/StitchNode.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StitchNode : NodeBaseClass {
+
+
+
+
+    public StitchNode(Rect r, int ID) : base(r,ID)
+    {
+
+    }
+
+    public  void DrawGUI(int winID)
+    { 
+               BaseDraw(winID);
+    }
+
+    public  void AttachComplete(int winID)
+    {
+        base.linkedNodes.Add(winID);
+    }
+}
